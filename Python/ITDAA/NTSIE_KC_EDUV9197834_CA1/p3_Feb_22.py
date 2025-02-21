bfb945a0c58ed3e56def03d80d5877eab294dd4e
Keatlegile Ntsie <busintsie@gmail.com>
2025-02-21 08:08:31 +0200

    update file

--- Python/ITDAA/NTSIE_KC_EDUV9197834_CA1/p3_Feb_22.py
+++ Python/ITDAA/NTSIE_KC_EDUV9197834_CA1/p3_Feb_22.py
@@ -3,8 +3,10 @@ import pandas as pd
 data = {
     "Name": ["Alice", "Bob","Charlie"],
     "Age": [25, 30 ,35],
-    "City": ["New York", "Los Angeles", "Chicago"]
+    "City": ["New York", "Los Angeles", "Chicago"],
+    "Salary": [35000, 45000, 29500]
 }
 
 df = pd.DataFrame(data)
-print(df)
+
+print(df["Age"])
